48613971eac6c3b8251934156ef29e62b66ba7c9
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-05-10 16:05:59 -0300

    atualizadas as class ClineteSimples, ClineteEspecial, AppSquadBank

---
+++ ProjetoSquadBank/src/tiposdeconta/ClienteEspecial.java
@@ -0,0 +1,12 @@
+package tiposdeconta;
+
+import src.contas.Conta;
+
+//ClienteEspecial herdará da class Conta
+public class ClienteEspecial extends Conta {
+
+	private String profissao;
+Private String cargo;
+	private double limite;
+
+}
---
+++ ProjetoSquadBank/src/tiposdeconta/ClienteSimples.java
@@ -0,0 +1,12 @@
+package tiposdeconta;
+
+import src.contas.Conta;
+
+//ClienteSimples herdará da class Conta
+public class ClienteSimples extends Conta{
+
+	private String endereco;
+	private String telefone;
+	private String email;
+	
+}
