37dbd7d03638f78192740678676c5fbc7e5e5b9e
lucileide <lucileidedacome@hotmail.com>
2022-05-10 11:16:30 -0300

    adição de biblioteca

--- ProjetoSquadBank/src/contas/Conta.java
+++ ProjetoSquadBank/src/contas/Conta.java
@@ -1,9 +1,15 @@
-package contas;
+package src.contas;
 
 public class Conta {
 	
 	private String cliente;
 	private String cpf;
 	private double saldo;
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
 
 }
