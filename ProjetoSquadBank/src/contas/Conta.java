3834641ba2a187a2c8153ebf511c040679176ccd
lucileide <lucileidedacome@hotmail.com>
2022-05-11 12:10:11 -0300

    Alteração da class conta

--- ProjetoSquadBank/src/contas/Conta.java
+++ ProjetoSquadBank/src/contas/Conta.java
@@ -5,4 +5,10 @@ public class Conta {
 	private String cliente;
 	private String cpf;
 	private double saldo;
+	
+	public Conta(String cliente, String cpf, double saldo) {
+		this.cliente = cliente;
+		this.cpf = cpf;
+		this.saldo = saldo;
+	}
 }
